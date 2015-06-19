0776aff0bfb2d6a7b8b44b1058f15eb734a64abd
Seung-Jun <seung-jun.lee@edu.esiee.fr>
2015-06-19 14:20:17 +0200

    buttons

---
+++ web/TestButton.php
@@ -0,0 +1,19 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="author" content="E-LOCKED TEAM">
+	<meta name="description" content="E-LOCKED PROJECT">
+	<meta name="keywords" content="lock, e-lock, cadenas, project">
+	<meta name="copyright" content="Tous droits reserves">
+	<meta name="subject" content="Projet E3 Cadenas Connecté">
+	<title>Essai Bouton</title>
+	
+	<link rel="stylesheet" type="text/css" href="StyleButton.css">
+</head>
+
+<body>
+	<a href="#" class="Connex">Connexion</a>
+	<a href="#" class="val">✓</a>
+	<a href="#" class="ref">✖</a>
+</body>
